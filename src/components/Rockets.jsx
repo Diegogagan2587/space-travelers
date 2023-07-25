2362002c900979c938e39336b8600043db4ba1ce
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-25 14:26:39 -0600

    Add Template for the Rockets.jsx content

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,9 +1,32 @@
 const Rockets = () => {
-    return (
-        <div>
-            <h1>Rockets</h1>
+  return (
+    <ul>
+      <li className="rocket-container">
+        <span>Picture would be here</span>
+        <div className="rocket-data">
+            <h2 className="name">Rocket Name</h2>
+            <p className="description">Description</p>
+            <button className="reserve-btn">Reserve Rocket</button>
         </div>
-    );
-}
+      </li>
+      <li className="rocket-container">
+        <span>Picture would be here</span>
+        <div className="rocket-data">
+            <h2 className="name">Rocket Name</h2>
+            <p className="description">Description</p>
+            <button className="cancel-btn">Cancel Reservation</button>
+        </div>
+      </li>
+      <li className="rocket-container">
+        <span>Picture would be here</span>
+        <div className="rocket-data">
+            <h2 className="name">Rocket Name</h2>
+            <p className="description">Description</p>
+            <button className="reserve-btn">Reserve Rocket</button>
+        </div>
+      </li>
+    </ul>
+  );
+};
 
 export default Rockets;

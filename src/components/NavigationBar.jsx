8fe53fd23fcf7aefc3ce8ab619dc3690512bad54
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-25 14:40:34 -0600

    Fix liners errors

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import '../styles/NavigationBar.css';
@@ -9,7 +8,7 @@ const NavigationBar = () => {
     <nav className='navigation-bar'>
       <div className='left'>
         <img src={heroIcon} alt="space travelers icon" className="hero-icon" />
-        <h1>Space Traveler's Hub</h1>
+        <h1>Space Traveler &#39;s Hub </h1>
       </div>
       <ul className='right'>
         <li>

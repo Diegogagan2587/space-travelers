deb089d4320ec16c7951e56219f1144e430930b1
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-25 10:24:33 -0600

    Import and initialize Routes and Route within the Navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import './App.css'
-import NavigationBar from './components/NavigationBar'
+import './App.css';
+import NavigationBar from './components/NavigationBar';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <Routes>
+        <Route path="/" element={<h1>Rockets</h1>} />
+        <Route path="/missions" element={<h1>Missions</h1>} />
+        <Route path="/my-profile" element={<h1>My Profile</h1>} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

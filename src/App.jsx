da5c59e6b5e36b6d8db4f36816d634d2dbb0f692
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-25 13:59:28 -0600

    Import Rockets Component, Add it as a Route within App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import NavigationBar from './components/NavigationBar';
 import { Routes, Route } from 'react-router-dom';
+import Rockets from './components/Rockets';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <NavigationBar />
       <Routes>
         <Route path="/" element={<h1>Rockets</h1>} />
-        <Route path="/rockets" element={<h1>Rockets</h1>} />
+        <Route path="/rockets" element={<Rockets/>} />
         <Route path="/missions" element={<h1>Missions</h1>} />
         <Route path="/my-profile" element={<h1>My Profile</h1>} />
       </Routes>

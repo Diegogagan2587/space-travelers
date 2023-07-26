ed61711d927aaf2f615e06bc91ef9c559b59e8d3
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-26 10:49:55 -0600

    Import rocketsReducer from rocketsSlice.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+import rocketsReducer from "./rockets/rocketsSlice.js";
 // import { combineReducers } from "redux/our-folder/ourSlice.js"; we need to import reducres from our slices
 
 const store = configureStore({

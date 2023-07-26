adcebe4469ee1ae1747f952bc1c2796e95eee4be
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-26 09:56:37 -0600

    Initialize the store

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+// import { combineReducers } from "redux"; we need to import reducres from our slices
+
+const store = configureStore({
+    reducer: {
+        // we need to add our reducers here
+    }
+})
+})
+
+export default store;
+```

58aa91e2d57672beafb9db022d50b5ba0db1ae1f
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-26 10:10:18 -0600

    add functio to fetch data from the endpont

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,6 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initalState = [];
+const endpoint = "https://api.spacexdata.com/v3/rockets";
+
+const fetchRockets = async () => {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    return data;
+}
+
+fetchRockets().then(data => console.log(data));
 
 const rocketsSlice = createSlice({
     name: "rockets",

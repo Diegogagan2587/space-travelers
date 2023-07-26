3c61b94b098066016ce5176004446d16d4af9519
dani <danifromecuador@gmail.com>
2023-07-26 13:12:11 -0500

    Add getMissions reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import { combineReducers } from "redux/our-folder/ourSlice.js"; we need to import reducres from our slices
+import getMissions from "./missions/missionsSlice.js";
 
 const store = configureStore({
     reducer: {
         // we need to add our reducers here
+        missions: getMissions,
     }
 })
 

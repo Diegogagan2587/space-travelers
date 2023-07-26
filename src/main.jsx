bbb007e63cd2499df42d9e7d2bdce816d42eff48
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-26 10:15:41 -0600

    Add reference comments

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
+//import Libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+//import componenets
 import App from './App.jsx';
 import store from './redux/store.js';
 import './index.css';

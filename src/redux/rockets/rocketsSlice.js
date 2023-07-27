88f521f9cbd43ede25f07e536b7d03ddf4503db3
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-26 20:54:19 -0600

    Add format to the code

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -14,21 +14,28 @@ const getRockets = createAsyncThunk('rockets/getRockets', fetchRockets);
 const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
-  reducers:{
-    reserveRocket: (state, action)=>{
-      const newState = state.map((rocket)=>{
-        if(rocket.id !== action.payload) return rocket;
+  reducers: {
+    reserveRocket: (state, action) => {
+      const newState = state.map((rocket) => {
+        if (rocket.id !== action.payload) return rocket;
         return { ...rocket, reserved: true };
-      })
-      state.splice(0,state.length,...newState);
-    }
+      });
+      state.splice(0, state.length, ...newState);
+    },
   },
   extraReducers: (builder) => {
     builder.addCase(getRockets.fulfilled, (state, action) => {
-      const newState = []
+      const newState = [];
       action.payload.forEach((rocket) => {
-        const { id, rocket_name, rocket_type, flickr_images, description } = rocket;
-        newState.push({ id, rocket_name, rocket_type, flickr_images, description });
+        const { id, rocket_name, rocket_type, flickr_images, description } =
+          rocket;
+        newState.push({
+          id,
+          rocket_name,
+          rocket_type,
+          flickr_images,
+          description,
+        });
       });
       state.splice(0, state.length, ...newState);
     });

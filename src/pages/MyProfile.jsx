a164c1986d7cfc3b86ad0e7906164b35d3f3b4be
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-26 23:32:59 -0600

    Add elemnts in componente: will render rockets names if reserved

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,6 +1,20 @@
+import { useSelector } from "react-redux";
+
 const MyProfile = () => {
-    return (
-        <div>My Profile Component</div>
-    )
-}
+    const rockets = useSelector((state)=>state.rockets);
+    console.log('rockets = ',rockets)
+  return (
+    <div className="my-profile-container">
+      <div className="reserved-rockets-container">
+        <ul className="rockets-list">
+            <h2>My Rockets</h2>
+            {rockets.map((rocket) => {
+                if(rocket.reserved) {return <li key={rocket.id}>{rocket.rocket_name}</li>}
+                return null
+            })}
+        </ul>
+      </div>
+    </div>
+  );
+};
 export default MyProfile;

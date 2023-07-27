ab04127dadeb3d8b41bce1b5422bf0faced0edba
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-27 16:00:15 -0600

    Install eslint-plugin-jest and Configure ESLint to use eslint-plugin-jest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,11 +6,12 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:jest/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh','jest'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',

be6f5b58b3bbe1988b5637ba4ab90c62fe19bbe0
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-28 15:20:17 -0600

    Implemente test for Rocket.jsx component: should render: name, description, cancel or reserve button

--- src/test/Rocket.test.jsx
+++ src/test/Rocket.test.jsx
@@ -25,4 +25,55 @@ describe('Renders Content', () => {
     const reserveBtn = screen.getByText('Reserve Rocket');
     expect(reserveBtn).toBeInTheDocument();
   });
+  test('If Reserved, Should render a Cancelation Button', () => {
+    const rocketData = {
+      rocket_name: 'Diego Rocket',
+      flickr_images: ['#'],
+      reserved: true,
+      description: 'Rocket create to test the React Component Rocket.jsx',
+      id: 27,
+      rocket_type: 'Cheaper Rocket',
+    };
+    const component = render(
+      <Provider store={store}>
+        <Rocket rocketProps={rocketData} />
+      </Provider>
+    );
+    const cancelBtn = screen.getByText('Cancel Reservation');
+    expect(cancelBtn).toBeInTheDocument();
+  });
+  test('if rocket_name: Diego Rocket, it should be rendered', () => {
+    const rocketData = {
+      rocket_name: 'Diego Rocket',
+      flickr_images: ['#'],
+      reserved: true,
+      description: 'Rocket create to test the React Component Rocket.jsx',
+      id: 27,
+      rocket_type: 'Cheaper Rocket',
+    };
+    const component = render(
+      <Provider store={store}>
+        <Rocket rocketProps={rocketData} />
+      </Provider>
+    );
+    const rocketName = screen.getByText('Diego Rocket');
+    expect(rocketName).toBeInTheDocument();
+  });
+  test('Description should be rendered: render: "This is my Description"', () => {
+    const rocketData = {
+      rocket_name: 'Diego Rocket',
+      flickr_images: ['#'],
+      reserved: true,
+      description: 'This is my Description',
+      id: 27,
+      rocket_type: 'Cheaper Rocket',
+    };
+    const component = render(
+      <Provider store={store}>
+        <Rocket rocketProps={rocketData} />
+      </Provider>
+    );
+    const rocketName = screen.getByText('Diego Rocket');
+    expect(rocketName).toBeInTheDocument();
+  });
 });

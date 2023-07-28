78027c1d5f34163622f52ef21dd1d97c52b00906
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-28 17:58:37 -0600

    Add styling for myProfile section

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -10,16 +10,16 @@ const MyProfile = () => {
   return (
     <div className="my-profile-container">
       <div className="reserved-rockets-container">
-        <ul className="rockets-list">
-          <h2>My Missions</h2>
+      <h2>My Missions</h2>
+        <ul className="reserved-list">
           {missions.filter((mission) => mission.reserved).map((mission) => {
             { return <li key={mission.id}>{mission.name}</li> }
           })}
         </ul>
       </div>
       <div className="reserved-rockets-container">
-        <ul className="rockets-list">
-          <h2>My Rockets</h2>
+      <h2>My Rockets</h2>
+        <ul className="reserved-list">
           {rockets.filter((rocket) => rocket.reserved).map((rocket) => {
             { return <li key={rocket.id}>{rocket.rocket_name}</li> }
           })}

01ea8c382126b0f5664f1395f20647dd712129b6
dani <danifromecuador@gmail.com>
2023-07-28 16:35:26 -0500

    Add test: dispatches the leaveMission action when leave button is clicked

--- src/test/Missions.test.jsx
+++ src/test/Missions.test.jsx
@@ -80,4 +80,39 @@ describe('Missions Component', () => {
     expect(actions).toHaveLength(1);
     expect(actions[0]).toEqual({ type: 'missions/joinMission', payload: 1 });
   });
+
+  it('dispatches the leaveMission action when leave button is clicked', () => {
+    // Define a mock state that resembles your real store state
+    const initialState = {
+      missions: {
+        missions: [
+          {
+            id: 1,
+            name: 'Mission 1',
+            description: 'Description for Mission 1',
+            reserved: true,
+          },
+        ],
+      },
+    };
+
+    // Create the mock store with the initial state
+    const store = mockStore(initialState);
+
+    // Render the component with the mocked store
+    render(
+      <Provider store={store}>
+        <Missions />
+      </Provider>
+    );
+
+    // Find the leave button and click it
+    const leaveButton = screen.getByText('Leave Mission');
+    fireEvent.click(leaveButton);
+
+    // Check that the leaveMission action was dispatched with the correct mission ID
+    const actions = store.getActions();
+    expect(actions).toHaveLength(1);
+    expect(actions[0]).toEqual({ type: 'missions/leaveMission', payload: 1 });
+  });
 });

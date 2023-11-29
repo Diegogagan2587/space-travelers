02bb2995cbd7d7771f9d8760abb7e8b2a20a36c5
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-28 19:20:14 -0600

    Setup vite for deployment on ./docs which is required by GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: './docs',
+  },
+  base: '/space-travelers/',
   plugins: [react()],
 })
